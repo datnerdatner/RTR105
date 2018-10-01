ca0c6e20102497b31edb4817cb4f427b89c89977
datner <datner@inbox.lv>
2018-10-01 17:47:08 +0300

    20181001_17_46

---
+++ test_1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+#http://www.cplusplus.com/reference/cstdio/printf/
+
+a = 10
+b = 1.99
+c = 'A'
+print("a = %d"%(a))
+print("b = %.4f"%(b))
+print("c = %s"%(c))
+a = 100500
+b = 5051.1337228
+c = 'A'
+print("a = %d"%(a))
+print("b = %.3f"%(b))
+print("c = %s"%(c))
